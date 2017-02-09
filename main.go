e1e3e45d3033eebdc1bfe13c9a41340315bb83b1
Alexandru Oprea <alexandru.oprea1989@gmail.com>
2017-02-09 22:21:20 +0200

    Implemented the extraction of the JSON root keys into separate arrays

--- main.go
+++ main.go
@@ -8,12 +8,18 @@ import (
 	"encoding/json"
 )
 
+type jsonInfo struct {
+	fileName string
+	jsonMap map[string]interface{}
+}
+
 func main() {
 	fileNames, err := getInputFileNames()
 	if err != nil {
 		panic(err)
 	}
 
+	jsonInfoArr := make([]jsonInfo, 0, len(fileNames))
 	for _, fileName := range fileNames {
 		fileBytes, err := ioutil.ReadFile(fileName)
 		if err != nil {
@@ -26,15 +32,42 @@ func main() {
 			panic(err)
 		}
 
-		fmt.Println(jsonMap)
+		ji := jsonInfo{
+			fileName: fileName,
+			jsonMap: jsonMap,
+		}
+
+		jsonInfoArr = append(jsonInfoArr, ji)
+	}
+
+	keys := getKeys(jsonInfoArr)
+	for k, v := range keys {
+		fmt.Println(fmt.Sprint("For the file %s, we jave the keys: ", k))
+		for _, field := range v {
+			fmt.Println(field)
+		}
+		fmt.Println("----------")
 	}
 }
 
 func getInputFileNames() ([]string, error) {
 	// Exclude the runnable argument
 	args := os.Args[1:]
-	if len(args) < 2 {
-		return nil, errors.New("You must provide at least two files")
+	if len(args) != 2 {
+		return nil, errors.New("You must provide two files")
 	}
 	return args, nil
+}
+
+func getKeys(jsonInfoArr []jsonInfo) map[string][]string {
+	keyMap := make(map[string][]string)
+	for _, ji := range jsonInfoArr {
+		length := len(ji.jsonMap)
+		keys := make([]string, 0, length)
+		for key, _ := range ji.jsonMap {
+			keys = append(keys, key)
+		}
+		keyMap[ji.fileName] = keys
+	}
+	return keyMap
 }
